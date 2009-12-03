f51e6a9e8701448e219fe382b10ee8b0f935317a
flunder <larsf2005@gmail.com>
2009-12-03 20:23:58 +0000

    pick/unpick works!

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -2,8 +2,6 @@ class Collection < ActiveRecord::Base
    belongs_to :user
    has_many :items
    
-   def self.find_or_create opts
-     first(:conditions => opts) || create(opts)
-   end
+
    
 end
