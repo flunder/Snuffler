125869bc6408cdf45926981f004d07f8a59bf475
flunder <larsf2005@gmail.com>
2009-12-03 19:36:37 +0000

    pick/unpick

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -2,18 +2,22 @@ class CollectionsController < ApplicationController
   
   def create
     
-     @collection = Collection.create(params[:collection]) 
+     @collection = Collection.find_or_create_by_item_id_and_user_id(params[:collection]) 
+    
+     #@collection = Collection.create(params[:collection]) 
+     
+     #@collection = Collection.create(:conditions => { :user_id => params[:user_id], :item_id => params[:item_id]}) 
+     #membership = Membership.find(:first, :conditions => { :user_id => params[:membership][:user_id], :neighbourhood_id =>  params[:membership][:neighbourhood_id]})
     
      respond_to do |format|
        if @collection.save
-         flash[:notice] = 'Item added!'
-         format.html { redirect_to(items_url) }
+         flash[:notice] = 'Added to favs!'
+         format.html { redirect_to(root_url) }
          format.xml  { render :xml => @collection, :status => :created, :location => @collection }
-
        else
-         format.html { render :action => "new" }
+         flash[:notice] = 'Sorry, something happened!'
+         #format.html { render :action => "new" }
          format.xml  { render :xml => @collection.errors, :status => :unprocessable_entity }
-
        end
      end
        
--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,4 +1,9 @@
 class Collection < ActiveRecord::Base
    belongs_to :user
    has_many :items
+   
+   def self.find_or_create opts
+     first(:conditions => opts) || create(opts)
+   end
+   
 end
