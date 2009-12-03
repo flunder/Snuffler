b538739a8052dcc25d97694b8d3b865c8beef6d8
flunder <larsf2005@gmail.com>
2009-12-03 13:48:25 +0000

    paperclip tweaks

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,24 +3,35 @@ require 'open-uri'
 class Item < ActiveRecord::Base
 
   belongs_to :user
-
+  
   # Paperclip
   has_attached_file :photo,
     :styles => {
       :thumb =>  ["100x100#", :png],
       :medium => ["200x200#", :png],
-      :large =>  ["700x700>", :png]}
+      :large =>  ["700x700>", :png]},
+    :path => ":rails_root/public/images/items/:id/:style/:basename.:extension",
+    :url  => "/images/items/:id/:style/:basename.:extension",
+    :default_url => "/images/empty.gif"      
       
   attr_accessor :image_url
   attr_accessor :image_remote_url
   
   validates_presence_of :name
+  before_create :dblcheck_file_name
   
   #has_attached_file :image
   before_validation :download_remote_image, :if => :image_url_provided?
   validates_presence_of :image_remote_url, :if => :image_url_provided?, :message => 'is invalid or inaccessible'
 
   private
+  
+    def dblcheck_file_name
+      #some fallback mechanism to fix the nofilename issue
+      if photo_file_name.nil?
+        self.photo.instance_write(:file_name, "#{ActiveSupport::SecureRandom.hex(16)}.png")
+      end
+    end
 
     def image_url_provided?
       !self.image_url.blank?
