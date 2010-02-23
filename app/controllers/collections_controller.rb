dfd3e97a370689ac08db86401c9fd6cd414b18e4
flunder <larsf2005@gmail.com>
2010-02-23 11:18:16 +0000

    tidy

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -2,45 +2,27 @@ class CollectionsController < ApplicationController
   
   def create
     
-    if request.xhr?
-           
-       collection = Collection.find(
-           :first, 
-           :conditions => { 
-             :user_id => params[:user_id], 
-             :item_id => params[:item_id]
-           })
-       
-      if collection
-        Collection.destroy(collection.id)
-      else
-        Collection.create(:user_id => params[:user_id], :item_id => params[:item_id])
-      end
-       
-       respond_to do |format| 
-         format.js { render(:partial => 'js/pick', :layout => false)}
-       end
-       
-    else
+    if request.xhr?        
       
       collection = Collection.find(
-          :first, 
-          :conditions => { 
-            :user_id => params[:collection][:user_id], 
-            :item_id => params[:collection][:item_id]
-          })
-     
-       if collection
-         Collection.destroy(collection.id)
-       else
-         Collection.create(params[:collection])
-       end
-     
-       respond_to do |format|
-         format.html { redirect_to(root_url) }
-       end
-     
+        :first, 
+        :conditions => { 
+          :user_id => params[:user_id], 
+          :item_id => params[:item_id]
+      })
+       
+      if collection
+        Collection.destroy(collection.id) #unpick
+      else
+        Collection.create(:user_id => params[:user_id], :item_id => params[:item_id]) #pick
+      end
+       
+      respond_to do |format| 
+        format.js { render(:partial => 'js/pick', :layout => false)} #ajax call
+      end
+
     end  
+    
     # respond_to do |format|
     #   if @collection.save
     #     flash[:notice] = 'Added to favs!' 
